7f311c7ca2f9d2f508aa1f5fbc99881c7b399d80
webP.deepika <deepikajamwal@endeavour.today>
2022-09-09 15:29:59 +0530

    added name and time of the message

--- websocket/chat_service/server.js
+++ websocket/chat_service/server.js
@@ -1,13 +1,23 @@
-import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    wss.clients.forEach(function each(client){
-        if(client!=ws && client.readyState==WebSocket.OPEN){
-            client.send(data);
-        }
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState == 1) {
+        let message_time = new Date(Date.now()).toLocaleTimeString();
+        client.send(message_time + " :  " + data);
+      }
     })
-  });
-});
+  })
+})
+
+server.listen(port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
